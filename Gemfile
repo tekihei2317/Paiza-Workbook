46e6b7b07c46c453a2818241e0313aad9f941db7
tekihei2317 <tekihei2317@gmail.com>
2020-09-22 14:15:57 +0900

    feat: Seleniumを導入

--- Gemfile
+++ Gemfile
@@ -73,3 +73,4 @@ gem 'jquery-rails'
 
 # scraping
 gem 'nokogiri'
+gem 'selenium-webdriver'
--- app/controllers/problems_controller.rb
+++ app/controllers/problems_controller.rb
@@ -14,7 +14,7 @@ class ProblemsController < ApplicationController
 
   def index
     # @problems = Problem.all
-    url = 'https://paiza.jp/challenges/ranks/d/info'
+    url = 'https://paiza.jp/challenges/ranks/d'
     @problems = scraping_problems(url)
   end
 
@@ -25,24 +25,29 @@ class ProblemsController < ApplicationController
   end
 
   def scraping_problems(url)
-    require 'open-uri'
-    charset = nil
-    html = open(url) do |f|
-      charset = f.charset
-      f.read
-    end
-    doc = Nokogiri::HTML.parse(html, nil, charset)
-
-    nodeset_titles = doc.css('.problem-box__header__title')
-    nodeset_difficulties = doc.css('.problem-box__bottom > dl > dd:nth-child(10) > b > span')
-
-    titles = nodeset_titles.map { |node| node.text.chomp }
-    difficulties = nodeset_difficulties.map { |node| node.text.to_i }
-
-    titles.zip(difficulties).map do |title, difficulty|
-      rank, number, name = parse_problem_title(title)
-      Problem.new(rank: rank, number: number, name: name, url: '  #', difficulty: difficulty)
-    end
+    # require 'open-uri'
+    # charset = nil
+    # html = open(url) do |f|
+    # charset = f.charset
+    # f.read
+    # end
+    # doc = Nokogiri::HTML.parse(html, nil, charset)
+    #
+    # nodeset_titles = doc.css('.problem-box__header__title')
+    # nodeset_difficulties = doc.css('.problem-box__bottom > dl > dd:nth-child(10) > b > span')
+    #
+    # titles = nodeset_titles.map { |node| node.text.chomp }
+    # difficulties = nodeset_difficulties.map { |node| node.text.to_i }
+    #
+    # binding.pry
+    #
+    # titles.zip(difficulties).map do |title, difficulty|
+    # rank, number, name = parse_problem_title(title)
+    # Problem.new(rank: rank, number: number, name: name, url: '  #', difficulty: difficulty)
+    # end
+    driver = Selenium::WebDriver.for :chrome
+    driver.get(url)
+    []
   end
 
   def parse_problem_title(title)
