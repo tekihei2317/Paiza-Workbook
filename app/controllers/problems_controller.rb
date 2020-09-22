0f55f0d4b900a2bad26178c7f9038c189eb92207
tekihei2317 <tekihei2317@gmail.com>
2020-09-22 11:23:24 +0900

    feat: 問題の情報を取得(ランク、番号、名前、難易度)
    リンクの取得はログインが必要そうだったので未解決

--- app/controllers/problems_controller.rb
+++ app/controllers/problems_controller.rb
@@ -13,12 +13,41 @@ class ProblemsController < ApplicationController
   end
 
   def index
-    @problems = Problem.all
+    # @problems = Problem.all
+    url = 'https://paiza.jp/challenges/ranks/d/info'
+    @problems = scraping_problems(url)
   end
 
   private
 
   def problem_params
     params.require(:problem).permit(:rank, :number, :name, :url, :difficulty)
+  end
+
+  def scraping_problems(url)
+    require 'open-uri'
+    charset = nil
+    html = open(url) do |f|
+      charset = f.charset
+      f.read
+    end
+    doc = Nokogiri::HTML.parse(html, nil, charset)
+
+    nodeset_titles = doc.css('.problem-box__header__title')
+    nodeset_difficulties = doc.css('.problem-box__bottom > dl > dd:nth-child(10) > b > span')
+
+    titles = nodeset_titles.map { |node| node.text.chomp }
+    difficulties = nodeset_difficulties.map { |node| node.text.to_i }
+
+    titles.zip(difficulties).map do |title, difficulty|
+      rank, number, name = parse_problem_title(title)
+      Problem.new(rank: rank, number: number, name: name, url: '  #', difficulty: difficulty)
+    end
+  end
+
+  def parse_problem_title(title)
+    regex = /(?<rank>[A-D,S])(?<number>\d{3}):(?<name>.*)/
+    m = regex.match(title)
+    [m[:rank], m[:number].to_i, m[:name]]
   end
 end
