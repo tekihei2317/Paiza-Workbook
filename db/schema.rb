20e9d341d24fc1ec8af10a41122004ae4ff9cce2
tekihei2317 <tekihei2317@gmail.com>
2020-09-24 16:41:01 +0900

    fix: usersテーブルが残っていたので削除して再作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_20_023217) do
+ActiveRecord::Schema.define(version: 2020_09_24_061238) do
 
   create_table "problems", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "rank"
@@ -18,6 +18,14 @@ ActiveRecord::Schema.define(version: 2020_09_20_023217) do
     t.string "name"
     t.string "url"
     t.integer "difficulty"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
