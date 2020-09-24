203ec630714782e2ef31d494cb8f605f3bbe707c
tekihei2317 <tekihei2317@gmail.com>
2020-09-24 13:13:10 +0900

    feat: ユーザー登録機能を実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,7 @@ class User < ApplicationRecord
   validates :name, presence: true
   validates :email, presence: true
   validates :password, presence: true
+
+  validates :name, uniqueness: true
+  validates :email, uniqueness: true
 end
