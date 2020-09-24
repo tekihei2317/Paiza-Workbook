5588688ac9149c7c45f07ffd767c5c50d10fd55a
tekihei2317 <tekihei2317@gmail.com>
2020-09-24 14:10:32 +0900

    fix: conflictが起こってるっぽい

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
-  validates :name, presence: true
-  validates :email, presence: true
-  validates :password, presence: true
-
-  validates :name, uniqueness: true
-  validates :email, uniqueness: true
+  # validates :name, presence: true
+  # validates :email, presence: true
+  # validates :password, presence: true
+  #
+  # validates :name, uniqueness: true
+  # validates :email, uniqueness: true
 end
