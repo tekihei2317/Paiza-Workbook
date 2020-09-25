7941dd0a116787cc2ed676c481f93cc8f3b26c0f
tekihei2317 <tekihei2317@gmail.com>
2020-09-25 13:53:00 +0900

    feat: Solvedモデルを作成(#17)
    Userモデル、Problemモデルとのアソシエーションを設定
    throughを用いてユーザーから解いた問題一覧を取得出来るように設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  has_many :solveds
+  has_many :solved_problems, through: :solveds, source: :problem
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   validates :name, presence: true
