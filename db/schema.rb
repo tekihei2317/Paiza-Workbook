69b7b8a2b7f78a290f2e803e3071dcd9d9054dc8
tekihei2317 <tekihei2317@gmail.com>
2020-10-01 21:19:47 +0900

    fix: enumで定義した値が取得できるようにカラムの型を変更

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_01_120742) do
+ActiveRecord::Schema.define(version: 2020_10_01_121542) do
 
   create_table "problems", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "rank"
+    t.integer "rank"
     t.integer "number"
     t.string "name"
     t.string "url"
