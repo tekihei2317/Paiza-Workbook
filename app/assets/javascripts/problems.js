bab8246aee8cf4ed3e27e8b73f1c029e24baf708
tekihei2317 <tekihei2317@gmail.com>
2020-10-05 01:06:17 +0900

    refactor: ソート処理をクラスに移動(#63)

--- app/assets/javascripts/problems.js
+++ app/assets/javascripts/problems.js
@@ -8,6 +8,14 @@ class ProblemsUtility {
   constructor(table, allProblems) {
     this.table = table;
     this.allProblems = allProblems;
+
+    // 初期状態のソートの向き(1: 昇順、0: ソートされていない、-1: 降順)
+    this.DEFAULT_SORT_DIRECTIONS = [1, 0, 0, 0, 0, 0];
+    // ソートされていないときに、どちらの向きにソートするか
+    this.FIRST_SORT_DIRECTIONS = [1, 1, 1, 1, - 1, -1];
+
+    // 現在のソートの向き(参照が入らないようにコピーしてから代入する)
+    this.currentSortDirections = this.DEFAULT_SORT_DIRECTIONS.concat();
   }
 
   // 問題の変更を反映する
@@ -15,6 +23,27 @@ class ProblemsUtility {
     // 一旦全部消去してから追加する
     this.allProblems.forEach((problem) => problem.remove());
     problems.forEach((problem) => this.table.appendChild(problem));
+  }
+
+  // 問題IDをランク・問題番号の大小が保たれるように数値に変換する
+  idToInt(id) {
+    const rankToInt = { D: 0, C: 1, B: 2, A: 3, S: 4 };
+    const rank = rankToInt[id[0]];
+    const number = Number(id.slice(1));
+    return rank * 1000 + number;
+  }
+
+  // 分:秒の形式の文字列を秒に変換する
+  timeToInt(id) {
+    const [m, s] = id.match(/\d+/g).map((num) => Number(num));
+    return m * 60 + s;
+  }
+
+  // カラムの値を数値に変換する
+  convertToNumber(value, index) {
+    if (index === 0) return this.idToInt(value);
+    if (index === 3) return this.timeToInt(value);
+    return Number(value);
   }
 
   // フィルタ処理をするイベントを設定する
@@ -46,12 +75,12 @@ class ProblemsUtility {
       this.applyProblems(filteredProblems);
 
       // ソートの状態を初期化する
-      // columnSortStates = INITIAL_SORT_STATES.concat();;
+      this.currentSortDirections = this.DEFAULT_SORT_DIRECTIONS.concat();
     });
   }
 
   // フォームの変更を検知して送信するイベントを設定する 
-  setSubmitEvent() {
+  setSubmitEvents() {
     const submitBtn = document.querySelector('input[type=submit]');
 
     // セレクト要素とチェックボックスを取り出す
@@ -63,117 +92,64 @@ class ProblemsUtility {
       target.addEventListener('change', () => submitBtn.click());
     });
   }
-}
 
-(() => {
-  let table = null;
-  let allProblems = null;
-
-  /**
-   * @type {ProblemsUtility} - 問題を管理するクラス
-   */
-  let util = null;
-
-  // 最初のカラムのソートの状態 [昇順, ソートされていない, 降順] = [1, 0, -1]
-  const INITIAL_SORT_STATES = [1, 0, 0, 0, 0, 0];
-  // 参照が代入されるので、コピーしてから代入する
-  let columnSortStates = INITIAL_SORT_STATES.concat();
-
-  // ソートされていないときに、どちらの方向にソートするか [昇順, 降順] = [1, -1]
-  const INITIAL_SORT_DIRECTIONS = [1, 1, 1, 1, -1, -1];
-  let firstSortDirections = INITIAL_SORT_DIRECTIONS.concat();
-
-  document.addEventListener('turbolinks:load', () => {
-    console.log('page loaded!');
-
-    table = document.querySelector('table');
-    // HTMLCollection->Array
-    allProblems = Array.from(document.getElementsByClassName('problem'));
-    util = new ProblemsUtility(table, allProblems);
-    setEvents();
-  });
-
-  function setEvents() {
-    util.setFilterEvent();
-    util.setSubmitEvent();
-    setSortEvent();
-  }
-
-  // IDでソート出来るように数値に変換する
-  function idToInt(id) {
-    const rankToInt = { D: 0, C: 1, B: 2, A: 3, S: 4 };
-    const rank = rankToInt[id[0]];
-    const number = Number(id.slice(1));
-    // ランクが等しい場合は問題番号の大小関係、
-    // ランクが異なる場合はランクの大小関係が保たれるような数値に変換する
-    return rank * 1000 + number;
-  }
-
-  // 秒に変換する
-  function timeToInt(id) {
-    const [m, s] = id.match(/\d+/g).map((num) => Number(num));
-    return m * 60 + s;
-  }
-
-  // ソート処理を設定する
-  function setSortEvent() {
-    // 難易度順のソート
+  // カラムをクリックしたときにソートするイベントを設定する
+  setSortEvents() {
     document.querySelector('tr').childNodes.forEach((th, index) => {
       // 名前のカラムではソートしない
       if (index === 1) return;
 
       th.addEventListener('click', () => {
-        console.log('th clicked!');
-        const currentProblems = Array.from(document.getElementsByClassName('problem'));
+        let currentProblems = Array.from(document.getElementsByClassName('problem'));
+        console.log(currentProblems.length);
 
-        // ソートされていない→最初のソートの向き、されている→逆向き
-        let sortDirection = firstSortDirections[index];
-        if (columnSortStates[index] !== 0) sortDirection = -columnSortStates[index];
-        columnSortStates[index] = sortDirection;
-
-        // ソートの状態を更新する
-        columnSortStates = columnSortStates.map((sortState, i) => {
-          return i === index ? sortDirection : 0;
-        });
+        // ソートされていない→最初のソートの向き、されている→逆向きにソートする
+        let nextDirection = this.FIRST_SORT_DIRECTIONS[index];
+        if (this.currentSortDirections[index] !== 0) nextDirection = -this.currentSortDirections[index];
 
         // ソートする
         currentProblems.sort((problemA, problemB) => {
-          valueA = problemA.childNodes[index].textContent;
-          valueB = problemB.childNodes[index].textContent;
+          let valueA = problemA.childNodes[index].textContent;
+          let valueB = problemB.childNodes[index].textContent;
 
           // ソートできるように数値に変換する
-          if (index === 0) {
-            // IDの場合
-            [valueA, valueB] = [idToInt(valueA), idToInt(valueB)];
-          } else if (index === 3) {
-            // 時間の場合
-            [valueA, valueB] = [timeToInt(valueA), timeToInt(valueB)];
-          } else {
-            // その他(数値)の場合
-            [valueA, valueB] = [Number(valueA), Number(valueB)];
-          }
-          return (valueA - valueB) * sortDirection;
+          valueA = this.convertToNumber(valueA, index);
+          valueB = this.convertToNumber(valueB, index);
+
+          return (valueA - valueB) * nextDirection;
+        });
+
+        // ソートの状態を更新する
+        this.currentSortDirections = this.currentSortDirections.map((direction, i) => {
+          return i === index ? nextDirection : 0;
         });
 
         // 変更を反映する
-        applyProblems(currentProblems);
+        this.applyProblems(currentProblems);
       });
-    })
-  }
-
-  // input要素、select要素の変更を検出して送信する
-  function setSubmitEvent() {
-    const submitBtn = document.querySelector('input[type=submit]');
-    console.log(submitBtn);
-
-    // セレクト要素とチェックボックスを取り出す
-    selectElems = Array.from(document.querySelectorAll('select'));
-    checkboxElems = Array.from(document.querySelectorAll('input[type=checkbox]'));
-    targets = selectElems.concat(checkboxElems);
-    console.log(targets);
-
-    targets.forEach((target) => {
-      target.addEventListener('change', () => submitBtn.click());
     });
+  }
+}
+
+(() => {
+  /**
+   * @type {ProblemsUtility} - 問題を管理するクラス
+   */
+  let util = null;
+
+  document.addEventListener('turbolinks:load', () => {
+    console.log('page loaded!');
+
+    const table = document.querySelector('table');
+    const allProblems = Array.from(document.getElementsByClassName('problem'));
+    util = new ProblemsUtility(table, allProblems);
+
+    setEvents();
+  });
+
+  function setEvents() {
+    util.setFilterEvent();
+    util.setSubmitEvents();
+    util.setSortEvents();
   }
 })();
