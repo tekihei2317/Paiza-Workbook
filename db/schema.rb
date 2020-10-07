a7dd6a475d80ab719cfe8f9cd6fb68670401f905
tekihei2317 <tekihei2317@gmail.com>
2020-10-07 14:49:01 +0900

    feat: usersモデルにpaiza_emailカラムを追加(#41)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_03_060559) do
+ActiveRecord::Schema.define(version: 2020_10_07_054714) do
 
   create_table "problems", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "rank"
@@ -46,6 +46,7 @@ ActiveRecord::Schema.define(version: 2020_10_03_060559) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.string "paiza_email"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
