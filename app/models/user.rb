c4d47a2ec5c23365899ee4bb349fddb2ba1f6b3b
tekihei2317 <tekihei2317@gmail.com>
2020-10-07 14:56:40 +0900

    fix: paizaのメールアドレスとパスワード無しでユーザー登録出来るように変更(#41)

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
   validates :name, presence: true
   validates :email, presence: true
   validates :password, presence: true
