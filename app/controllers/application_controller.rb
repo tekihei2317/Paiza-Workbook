87c6780ba893a187d569dd2a6701142ed6ea120b
tekihei2317 <tekihei2317@gmail.com>
2020-10-07 16:35:33 +0900

    feat: 名前とPaizaの情報も変更できるように設定(#41)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,7 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :paiza_email, :paiza_password])
   end
 
   private
