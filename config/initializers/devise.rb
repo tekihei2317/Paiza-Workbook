b66651ca53a3cf7d15b94243e7ac60919d546ac6
tekihei2317 <tekihei2317@gmail.com>
2020-10-08 14:08:31 +0900

    feat: omniauthを使えるようにモデルとルーティングを設定(#70)

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -308,4 +308,6 @@ Devise.setup do |config|
   # When set to false, does not sign a user in automatically after their password is
   # changed. Defaults to true, so a user is signed in automatically after changing a password.
   # config.sign_in_after_change_password = true
+
+  config.omniauth :twitter, 'SCWpwXH4LKn2d7jZqZMhgU3hG', 'p9bdgnVirKRgaVq4tLjU6DftkgKeNtTS2qWPhjFSinyGA9roXl'
 end
