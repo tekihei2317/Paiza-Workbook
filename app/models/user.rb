3d9348fee7dad40774897a332035dfb03fed21a1
tekihei2317 <tekihei2317@gmail.com>
2020-10-08 11:35:12 +0900

    fix: Paizaの情報が登録されていない時に例外が出ないように修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,6 @@ class User < ApplicationRecord
   validates :name, presence: true
   validates :email, presence: true
   # validates :password, presence: true
-  validates :paiza_password, presence: true
 
   validates :name, uniqueness: true
   validates :email, uniqueness: true
