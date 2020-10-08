17a1d616726e442e607fe4d00b6737a117560582
tekihei2317 <tekihei2317@gmail.com>
2020-10-08 14:30:16 +0900

    refactor: 認証用の関数を修正(#70)

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -29,13 +29,19 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   # end
 
   def twitter
-    @user = User.find_for_twitter_oauth(request.env['omniauth.auth'], current_user)
+    call_back_from :twitter
+  end
+
+  def callback_from(provider)
+    provider = provider.to_s
+
+    @user = User.find_for_oauth(request.env['omniauth.auth'])
 
     if @user.persisted?
-      set_flash_message(:notice, :success, :kind => 'Twitter') if is_navigational_format?
-      sign_in_and_redirect @user, :event => :authentication
+      flash[:notice] = I18n.t('devise.omniauth_callbacks.success', kind: provider.capitalize)
+      sign_in_and_redirect @user, event: :authentication
     else
-      session['devise.twitter_data'] = request.env['omniauth.auth'].except('extra')
+      session["devise.#{provider}_data"] = request.env['omniauth.auth']
       redirect_to new_user_registration_url
     end
   end
