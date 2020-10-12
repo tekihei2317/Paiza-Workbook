a07da2be4479df4706365a07c7fb4deca7dfb4d3
tekihei2317 <tekihei2317@gmail.com>
2020-10-12 19:09:21 +0900

    refactor: Problemモデルのバリデーションのテストをカラムごとに整理(#81)

--- spec/models/problem_spec.rb
+++ spec/models/problem_spec.rb
@@ -1,83 +1,95 @@
 require 'rails_helper'
 
 RSpec.describe Problem, type: :model do
-  it 'ランク、問題番号、問題名、URL、難易度があれば有効である' do
-    problem = Problem.new(
-      rank: 'D',
-      number: 166,
-      name: '何日後になるか',
-      url: 'https://paiza.jp/challenges/405/ready',
-      difficulty: 847,
-    )
-    expect(problem).to be_valid
-  end
+  describe 'バリデーション' do
+    it 'ランク、問題番号、問題名、URL、難易度があれば有効である' do
+      problem = Problem.new(
+        rank: 'D',
+        number: 166,
+        name: '何日後になるか',
+        url: 'https://paiza.jp/challenges/405/ready',
+        difficulty: 847,
+      )
+      expect(problem).to be_valid
+    end
 
-  it 'ランクがなければ無効である' do
-    problem = Problem.new(rank: nil)
-    problem.valid?
-    expect(problem.errors[:rank]).to include('を入力してください')
-  end
+    context 'rankカラム' do
+      it '存在しなければ無効である' do
+        problem = Problem.new(rank: nil)
+        problem.valid?
+        expect(problem.errors[:rank]).to include('を入力してください')
+      end
+    end
 
-  it '問題番号がなければ無効である' do
-    problem = Problem.new(number: nil)
-    problem.valid?
-    expect(problem.errors[:number]).to include('を入力してください')
-  end
+    context 'numberカラム' do
+      it '存在しなければ無効である' do
+        problem = Problem.new(number: nil)
+        problem.valid?
+        expect(problem.errors[:number]).to include('を入力してください')
+      end
+    end
 
-  it '問題名がなければ無効である' do
-    problem = Problem.new(name: nil)
-    problem.valid?
-    expect(problem.errors[:name]).to include('を入力してください')
-  end
+    context 'nameカラム' do
+      it '存在しなければ無効である' do
+        problem = Problem.new(name: nil)
+        problem.valid?
+        expect(problem.errors[:name]).to include('を入力してください')
+      end
 
-  it 'URLがなければ無効である' do
-    problem = Problem.new(url: nil)
-    problem.valid?
-    expect(problem.errors[:url]).to include('を入力してください')
-  end
+      it '一意である(同じ名前の問題を複数登録できない)' do
+        Problem.create(
+          rank: 'D',
+          number: 166,
+          name: '何日後になるか',
+          url: 'https://paiza.jp/challenges/405/ready',
+          difficulty: 847,
+        )
+        problem = Problem.new(
+          rank: 'D',
+          number: 166,
+          name: '何日後になるか',
+          url: 'https://paiza.jp/challenges/405/ready',
+          difficulty: 847,
+        )
+        problem.valid?
+        expect(problem.errors[:name]).to include('はすでに存在します')
+      end
+    end
 
-  it '難易度がなければ無効である' do
-    problem = Problem.new(difficulty: nil)
-    problem.valid?
-    expect(problem.errors[:difficulty]).to include('を入力してください')
-  end
+    context 'urlカラム' do
+      it '存在しなければ無効である' do
+        problem = Problem.new(url: nil)
+        problem.valid?
+        expect(problem.errors[:url]).to include('を入力してください')
+      end
 
-  it '同じ名前の問題を複数登録できない' do
-    Problem.create(
-      rank: 'D',
-      number: 166,
-      name: '何日後になるか',
-      url: 'https://paiza.jp/challenges/405/ready',
-      difficulty: 847,
-    )
-    problem = Problem.new(
-      rank: 'D',
-      number: 166,
-      name: '何日後になるか',
-      url: 'https://paiza.jp/challenges/405/ready',
-      difficulty: 847,
-    )
-    problem.valid?
-    expect(problem.errors[:name]).to include('はすでに存在します')
-  end
+      it '一意である(同じURLの問題を複数登録できない)' do
+        Problem.create(
+          rank: 'D',
+          number: 166,
+          name: '何日後になるか',
+          url: 'https://paiza.jp/challenges/405/ready',
+          difficulty: 847,
+        )
+        problem = Problem.new(
+          rank: 'D',
+          number: 166,
+          name: '何日後になるか',
+          url: 'https://paiza.jp/challenges/405/ready',
+          difficulty: 847,
+        )
+        problem.valid?
+        expect(problem.errors[:url]).to include('はすでに存在します')
+      end
+    end
 
-  it '同じURLの問題を複数登録できない' do
-    Problem.create(
-      rank: 'D',
-      number: 166,
-      name: '何日後になるか',
-      url: 'https://paiza.jp/challenges/405/ready',
-      difficulty: 847,
-    )
-    problem = Problem.new(
-      rank: 'D',
-      number: 166,
-      name: '何日後になるか',
-      url: 'https://paiza.jp/challenges/405/ready',
-      difficulty: 847,
-    )
-    problem.valid?
-    expect(problem.errors[:url]).to include('はすでに存在します')
+    context 'difficultyカラム' do
+      it '存在しなければ無効である' do
+        problem = Problem.new(difficulty: nil)
+        problem.valid?
+        expect(problem.errors[:difficulty]).to include('を入力してください')
+      end
+    end
   end
 
   # インスタンスメソッドのテスト
