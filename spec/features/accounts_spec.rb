3630c5fe667fc20eb2b56e27540ad8b4016951bc
tekihei2317 <tekihei2317@gmail.com>
2020-10-13 17:17:04 +0900

    feat: アカウント登録機能とログイン機能のフィーチャースペックを実装(#83)

---
+++ spec/features/accounts_spec.rb
@@ -0,0 +1,64 @@
+require 'rails_helper'
+
+RSpec.feature 'Accounts', type: :feature do
+  # Capybara.default_driver = :selenium_chrome
+
+  describe 'アカウント登録機能' do
+    scenario '有効な属性の場合は登録できる' do
+      user = FactoryBot.build(:user)
+
+      visit new_user_registration_path
+
+      expect {
+        fill_in 'Username', with: user.name
+        fill_in 'E-mail', with: user.email
+        fill_in 'Password', with: user.password
+        fill_in 'Password Confirmation', with: user.password
+        click_button 'SIGN UP'
+      }.to change(User, :count).by(1)
+
+      # 問題一覧画面にリダイレクトされる
+      expect(page).to have_current_path root_path
+    end
+
+    scenario '無効な属性の場合は登録できない' do
+      user = FactoryBot.build(:user, :invalid_password)
+
+      visit new_user_registration_path
+
+      expect {
+        fill_in 'Username', with: user.name
+        fill_in 'E-mail', with: user.email
+        fill_in 'Password', with: user.password
+        fill_in 'Password Confirmation', with: user.password
+        click_button 'SIGN UP'
+      }.to_not change(User, :count)
+
+      # リダイレクトされない
+      expect(page).to_not have_current_path root_path
+    end
+  end
+
+  describe 'ログイン機能' do
+    scenario 'メールアドレスでログインすることができる' do
+      user = FactoryBot.create(:user)
+
+      visit root_path
+      click_link 'login'
+      expect(page).to have_current_path new_user_session_path
+
+      fill_in 'E-mail', with: user.email
+      fill_in 'Password', with: user.password
+      click_button 'SIGN IN'
+      expect(page).to have_current_path root_path
+    end
+  end
+
+  describe 'ログアウト機能' do
+    before do
+    end
+
+    scenario 'ログアウトすることができる' do
+    end
+  end
+end
