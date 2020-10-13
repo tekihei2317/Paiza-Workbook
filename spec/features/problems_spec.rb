768a6ce478c0619792a4727e14d4c4d8ba78a56c
tekihei2317 <tekihei2317@gmail.com>
2020-10-13 16:19:51 +0900

    feat: フィーチャースペックを書く準備をしました(#83)
    test環境でCSSとJSが更新されないのは、
    config.cache_classesをfalseにしたら直りました

--- spec/features/problems_spec.rb
+++ spec/features/problems_spec.rb
@@ -1,7 +1,25 @@
 require 'rails_helper'
 
-RSpec.feature 'Problems', type: :feature do
-  scenario 'ユーザーはログイン出来る' do
+RSpec.feature 'Problems', type: :feature, js: true do
+  # scenario 'ユーザーはログイン出来る' do
+  # user = FactoryBot.create(:user)
+  #
+  # visit root_path
+  # click_link 'login'
+  # expect(page).to have_current_path new_user_session_path
+  #
+  # fill_in 'E-mail', with: user.email
+  # fill_in 'Password', with: user.password
+  # click_button 'SIGN IN'
+  # expect(page).to have_current_path root_path
+  # end
+  #
+  # scenario 'ユーザーが問題IDでソートする' do
+  # visit root_path
+  # click '問題ID'
+  # end
+
+  scenario 'ユーザーがDランクのチェックボックスを操作する' do
     user = FactoryBot.create(:user)
 
     visit root_path
@@ -12,5 +30,9 @@ RSpec.feature 'Problems', type: :feature do
     fill_in 'Password', with: user.password
     click_button 'SIGN IN'
     expect(page).to have_current_path root_path
+
+    binding.pry
+    uncheck 'rank_d'
+    binding.pry
   end
 end
