5b8435fceaa326fb1fa730a0700595919a9b8f7b
tekihei2317 <tekihei2317@gmail.com>
2020-10-13 14:21:03 +0900

    feat: Solvedモデルのテストを実装(#81)

--- spec/factories/problems.rb
+++ spec/factories/problems.rb
@@ -5,5 +5,13 @@ FactoryBot.define do
     name { '何日後になるか' }
     url { 'https://paiza.jp/challenges/405/ready' }
     difficulty { 847 }
+
+    factory :other_problem do
+      rank { 'B' }
+      number { 21 }
+      name { '複数形への変換' }
+      url { 'https://paiza.jp/challenges/77/ready' }
+      difficulty { 1592 }
+    end
   end
 end
