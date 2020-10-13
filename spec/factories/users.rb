b0a278dc963e7c474a4b6bc02072e99e2f182a3f
tekihei2317 <tekihei2317@gmail.com>
2020-10-13 13:12:48 +0900

    refactor: Problemモデルのテストをファクトリで書き換え(#81)

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,7 +1,8 @@
 FactoryBot.define do
   factory :user do
     name { 'Alice' }
-    email { 'alice@example.com' }
+    # email { 'alice@example.com' }
+    sequence(:email) { |i| "tester#{i}@example.com" }
     password { 'hogehoge' }
   end
 end
