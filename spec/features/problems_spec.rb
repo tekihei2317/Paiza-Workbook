a91f1523b2cc13be81f8466d178db00068229d16
tekihei2317 <tekihei2317@gmail.com>
2020-10-14 16:08:01 +0900

    feat: 問題ソート機能のフィーチャースペックを実装(#83)

--- spec/features/problems_spec.rb
+++ spec/features/problems_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.feature 'Problems', type: :feature, js: true do
+RSpec.feature 'Problems', type: :feature do
   scenario '問題が全問表示されている' do
     visit root_path
     problems = all(:css, 'tr.problem')
