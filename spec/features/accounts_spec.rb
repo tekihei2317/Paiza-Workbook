cbe91ea4d48a60a8b3e316e926f4f4c1f1abdc24
tekihei2317 <tekihei2317@gmail.com>
2020-10-14 13:02:37 +0900

    feat: アカウント削除機能のフィーチャースペックを実装(#83)

--- spec/features/accounts_spec.rb
+++ spec/features/accounts_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.feature 'Accounts', type: :feature do
-  # Capybara.default_driver = :selenium_chrome
+  Capybara.default_driver = :selenium_chrome
 
   describe 'アカウント登録機能' do
     scenario '有効な属性の場合は登録できる' do
@@ -127,6 +127,34 @@ RSpec.feature 'Accounts', type: :feature do
 
       # 更新されていないことを確認する
       expect(@user.reload.name).to eq 'Alice'
+    end
+  end
+
+  describe 'アカウント削除機能' do
+    before do
+      @user = FactoryBot.create(:user)
+      visit new_user_session_path
+      fill_in 'E-mail', with: @user.email
+      fill_in 'Password', with: @user.password
+      click_button 'SIGN IN'
+
+      # トーストをクリックして消す
+      find(:css, 'div.toast').click
+    end
+
+    scenario 'アカウントを削除することが出来る' do
+      click_link 'profile'
+      click_link '編集する'
+
+      expect {
+        click_link 'アカウントを削除する'
+
+        # Rask::TestではJavaScriptが動作しないので、ダイアログは出ないみたい
+        # https://ja.stackoverflow.com/questions/66340/rspec%E3%81%A7confirm%E3%83%80%E3%82%A4%E3%82%A2%E3%83%AD%E3%82%B0%E3%81%AE%E3%83%86%E3%82%B9%E3%83%88%E3%81%8C%E9%80%9A%E3%82%89%E3%81%AA%E3%81%84
+        page.accept_confirm
+
+        expect(page).to have_content 'アカウントを削除しました'
+      }.to change(User, :count).by(-1)
     end
   end
 end
