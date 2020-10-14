93e6be57f012334f750a69ac6301839c698458d0
tekihei2317 <tekihei2317@gmail.com>
2020-10-14 16:18:56 +0900

    fix: Problemのファクトリが一意性で落ちていたので修正(#83)

--- spec/factories/problems.rb
+++ spec/factories/problems.rb
@@ -1,17 +1,18 @@
 FactoryBot.define do
+  # テストのために実際の問題は登録しているので、架空の問題を使う
   factory :problem do
-    rank { 'D' }
-    number { 166 }
-    name { '何日後になるか' }
-    url { 'https://paiza.jp/challenges/405/ready' }
-    difficulty { 847 }
+    rank { 'S' }
+    number { 999 }
+    name { '団長からの挑戦状' }
+    url { 'https://paiza.jp/challenges/9999/ready' }
+    difficulty { 9999 }
 
     factory :other_problem do
-      rank { 'B' }
-      number { 21 }
-      name { '複数形への変換' }
-      url { 'https://paiza.jp/challenges/77/ready' }
-      difficulty { 1592 }
+      rank { 'S' }
+      number { 888 }
+      name { '師匠からの試練' }
+      url { 'https://paiza.jp/challenges/8888/ready' }
+      difficulty { 8888 }
     end
   end
 end
