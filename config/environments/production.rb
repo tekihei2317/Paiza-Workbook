6a3dc9ce239f41b23cefad55485292ec215e7b64
tekihei2317 <tekihei2317@gmail.com>
2020-10-19 17:28:32 +0900

    fix: エラーが出たのでhttpsの設定も追記

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,6 +46,7 @@ Rails.application.configure do
   # config.action_cable.url = 'wss://example.com/cable'
   # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
   config.action_cable.allowed_request_origins = ['http://paizaworkbook.com', /http:\/\/paizaworkbook.*/]
+  config.action_cable.allowed_request_origins = ['https://paizaworkbook.com', /https:\/\/paizaworkbook.*/]
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
 
