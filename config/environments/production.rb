4ed77d5b82d7e16218ba600a2b3c1aea66d77dba
tekihei2317 <tekihei2317@gmail.com>
2020-10-19 17:53:59 +0900

    fix: force_sslを追記

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -45,10 +45,10 @@ Rails.application.configure do
   # config.action_cable.mount_path = nil
   # config.action_cable.url = 'wss://example.com/cable'
   # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
-  config.action_cable.allowed_request_origins = ['http://paizaworkbook.com', /http:\/\/paizaworkbook.*/]
+  # config.action_cable.allowed_request_origins = ['http://paizaworkbook.com', /http:\/\/paizaworkbook.*/]
   config.action_cable.allowed_request_origins = ['https://paizaworkbook.com', /https:\/\/paizaworkbook.*/]
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  # config.force_ssl = true
+  config.force_ssl = true
 
   # Use the lowest log level to ensure availability of diagnostic information
   # when problems arise.
