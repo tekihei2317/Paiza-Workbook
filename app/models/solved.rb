d444c8f19cf2b2d546a157babcda3457fdb282fc
tekihei2317 <tekihei2317@gmail.com>
2020-10-29 11:47:37 +0900

    feat: solvedsテーブルにスコアを保存するカラムを追加(#100)

--- app/models/solved.rb
+++ app/models/solved.rb
@@ -4,5 +4,6 @@ class Solved < ApplicationRecord
 
   validates :user_id, presence: true
   validates :problem_id, presence: true
+  validates :first_score, presence: true
   validates :user_id, uniqueness: { scope: :problem_id }
 end
