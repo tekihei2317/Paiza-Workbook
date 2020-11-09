65886d84289795b6a714e9130385ceb6bf9fa26d
tekihei2317 <tekihei2317@gmail.com>
2020-11-09 14:37:42 +0900

    fix: Solvedモデルの変更に伴いバリデーションを修正(#100)

--- app/models/solved.rb
+++ app/models/solved.rb
@@ -4,6 +4,4 @@ class Solved < ApplicationRecord
 
   validates :user_id, presence: true
   validates :problem_id, presence: true
-  validates :first_score, presence: true
-  validates :user_id, uniqueness: { scope: :problem_id }
 end
