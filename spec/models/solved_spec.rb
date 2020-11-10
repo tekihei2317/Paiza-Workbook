f3cfdcdbfe08e9e6190d2fb5f4834093bfeb99a2
tekihei2317 <tekihei2317@gmail.com>
2020-11-10 14:10:35 +0900

    fix: Solvedモデルのテストを修正(#100)

--- spec/models/solved_spec.rb
+++ spec/models/solved_spec.rb
@@ -35,22 +35,6 @@ RSpec.describe Solved, type: :model do
       end
     end
 
-    context 'first_socreカラム' do
-      it '存在しなければ無効である' do
-        solved = FactoryBot.build(:solved, user: @user, problem: @problem, first_score: nil)
-        solved.valid?
-        expect(solved.errors[:first_score]).to include('を入力してください')
-      end
-    end
-
-    it '同じユーザーは同じ問題を２回解くことが出来ない' do
-      FactoryBot.create(:solved, user: @user, problem: @problem)
-      solved = FactoryBot.build(:solved, user: @user, problem: @problem)
-      solved.valid?
-      expect(solved).to_not be_valid
-      expect(solved.errors[:user_id]).to include('はすでに存在します')
-    end
-
     it '同じユーザーは別の問題を解くことが出来る' do
       FactoryBot.create(:solved, user: @user, problem: @problem)
       solved = FactoryBot.create(:solved, user: @user, problem: @other_problem)
